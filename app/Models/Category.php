881778fce180076767e47614d1e0311cbc621cee
Sergey <gavrilenko.php@gmail.com>
2025-02-17 16:58:22 +0200

    (3) updated data structure according to real data (commit should be rebased later)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
 {
     /** @var string[]  */
     protected $fillable = [
-        'title',
+        'name',
     ];
 
     /**
--- database/migrations/2025_02_17_130836_create_books_table.php
+++ database/migrations/2025_02_17_130836_create_books_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
-            $table->integer('isbn');
+            $table->string('isbn')->nullable();
             $table->integer('count_of_pages');
-            $table->date('published_date');
+            $table->date('published_date')->nullable();
             $table->string('thumbnail_url')->nullable();
-            $table->string('short_description')->nullable();
+            $table->text('short_description')->nullable();
             $table->text('long_description')->nullable();
             $table->string('status', 30)->nullable();
             $table->timestamps();
